3b4871fe45affaf0362fd95ed6bd2e5eee97089a
Chris Morales Barcelo <csmorales2002@gmail.com>
2024-09-20 01:22:28 -0500

    Avatar Nav

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import { AppBar, Toolbar, Typography, Container, Button, } from '@mui/material';
+import { AppBar, Toolbar, Typography, Container, Button, Avatar, Menu, MenuItem, IconButton,} from '@mui/material';
 
 
 import Home from './components/Home';
@@ -11,7 +11,25 @@ import AdminLogin from './components/AdminBoard';
 import EventManagement from './components/EventManagement';
 import UserProfile from './components/UserProfileManagement';
 
+
 function App() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const user= {
+    name: 'John Doe',
+    email: 'john.doe@example.com',
+    avatar: 'https://randomuser.me/api',
+  };
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+
   return (
     <Router>
      
@@ -24,6 +42,20 @@ function App() {
           <Button color="inherit" component={Link} to="/volunteer">Volunteer Events</Button>
           <Button color="inherit" component={Link} to="/login">Login</Button>
           
+          <IconButton onClick={handleClick} color='inherit'>
+            <Avatar src={user.avatar} alt={user.name} />
+          </IconButton>
+          <Menu
+            anchorEl={anchorEl}
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+            transformOrigin={{horizontal: 'right',vertical: 'top'}}
+            anchorOrigin={{horizontal:'right',vertical: 'bottom'}}
+          >
+            <MenuItem> Notifications </MenuItem>
+            <MenuItem> History </MenuItem>
+            <MenuItem onClick={handleClose}> Logout </MenuItem>
+          </Menu>
           
         </Toolbar>
       </AppBar>
