04649bd42cd8964157019dc61ffc215f10c9acbb
Rikkayyy <144562818+Rikkayyy@users.noreply.github.com>
2024-10-18 13:19:10 -0500

    implemented button for event creation

--- frontend/src/components/AdminBoard.js
+++ frontend/src/components/AdminBoard.js
@@ -1,12 +1,27 @@
 import React from 'react';
-import { Typography } from '@mui/material';
+import { Typography, Button } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 
 function AdminBoard() {
+  const navigate = useNavigate();
+
+  const handleEventManagementClick = () => {
+    navigate('/event-management');
+  };
+
   return (
     <div>
       <Typography variant="h4" gutterBottom>
         Admin Dashboard Page
       </Typography>
+      
+      <Button 
+        variant="contained" 
+        color="primary" 
+        onClick={handleEventManagementClick}
+      >
+        Create Events
+      </Button>
 
     </div>
   );
