db8eefe5eb83eccc2a487783d3a32ec139cfc2c1
Rikkayyy <144562818+Rikkayyy@users.noreply.github.com>
2024-10-19 08:31:55 -0500

    reformatted app.js to match main

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const cors = require("cors");
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// Importing routes
 const authRoutes = require("./routes/authRoutes");
 const eventRoutes = require("./routes/eventRoutes");
 const historyRoutes = require("./routes/historyRoutes");
@@ -14,18 +14,14 @@ const volunteerRoutes = require("./routes/volunteerRoutes");
 const profileRoutes = require("./routes/profileRoutes");
 const notificationRoutes = require("./routes/notificationRoutes");
 
-// Using routes with an API prefix for organization
-app.use("/notifications", notificationRoutes);
-app.use("/auth", authRoutes);
-app.use("/events", eventRoutes);
-app.use("/history", historyRoutes);
-app.use("/sign-up", signUpRoutes);
-app.use("/volunteers", volunteerRoutes);
-app.use("/profiles", profileRoutes);
 
-// Catch-all error handler for routes that do not exist
-app.use((req, res) => {
-  res.status(404).json({ message: "Route not found" });
-});
+
+app.use("/", notificationRoutes);
+app.use("/", authRoutes);
+app.use("/", eventRoutes);
+app.use("/", historyRoutes);
+app.use("/", signUpRoutes);
+app.use("/", volunteerRoutes);
+app.use("/", profileRoutes);
 
 module.exports = app;
