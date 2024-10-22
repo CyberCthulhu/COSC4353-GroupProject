83832e59b099893633e62da9204e9daed647b940
Chris Morales Barcelo <csmorales2002@gmail.com>
2024-10-22 04:53:56 -0500

    mongoDB database connected

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,25 @@
 const app = require("./app");
 const cron = require("node-cron"); 
+const mongoose = require("mongoose");
 const { notifyVolunteers } = require("./services/notificationService"); 
 const { updateParticipation } = require("./services/updateParticipationService");
 const PORT = 4000;
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+
+
+
+mongoose.connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to the database!");
+  })
+  .catch((error) => {
+    console.log("Connection failed!", error);
+  });
+
+
 
 app.get("/", (req, res) => {
   res.send("Backend is running");
