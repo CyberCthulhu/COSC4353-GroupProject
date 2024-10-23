27a0d8425b2ee286ad5bf12250e1cd2c421998d4
Chris Morales Barcelo <csmorales2002@gmail.com>
2024-10-22 23:54:11 -0500

    participation history working

--- backend/controllers/historyController.js
+++ backend/controllers/historyController.js
@@ -1,12 +1,23 @@
-const userParticipation = require("../models/userParticipationModel");
-const {events} = require("../models/eventModel");
+const UserParticipation = require("../models/userParticipationModel");
+const Event = require("../models/eventModel");
 
-exports.getHistory = (req, res) => {
+exports.getHistory = async (req, res) => {
   const userId = req.params.userId;
-  if (!userParticipation[userId]) {
-    return res.status(404).json({ message: "No events have been participated in" });
+
+  try {
+    const userParticipation = await UserParticipation.findOne({ userId });
+
+    if (!userParticipation) {
+      return res.status(404).json({ message: "No events have been participated in" });
+    }
+
+    const eventIds = userParticipation.eventIds;
+
+    const userEvents = await Event.find({ _id: { $in: eventIds } });
+
+    res.json(userEvents);
+  } catch (error) {
+    console.error("Error retrieving user history:", error);
+    res.status(500).json({ message: "Error retrieving user history", error });
   }
-  const eventIds = userParticipation[userId];
-  const userEvents = events.filter((event) => eventIds.includes(event.id));
-  res.json(userEvents);
 };
--- backend/models/userParticipationModel.js
+++ backend/models/userParticipationModel.js
@@ -1,7 +1,20 @@
-const userParticipation = {
-  1: [1, 2, 3, 4],
-  2: [3],
-  3: [4],
-};
+// const userParticipation = {
+//   1: [1, 2, 3, 4],
+//   2: [3],
+//   3: [4],
+// };
 
-module.exports = userParticipation;
+// module.exports = userParticipation;
+
+const mongoose = require('mongoose');
+
+const userParticipationSchema = new mongoose.Schema({
+  userId: { type: String, required: true },
+  eventIds: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Event' }],
+}, {
+  timestamps: true,
+});
+
+const UserParticipation = mongoose.model('UserParticipation', userParticipationSchema);
+
+module.exports = UserParticipation;
