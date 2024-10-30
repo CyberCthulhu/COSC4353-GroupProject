2165512421065a4fba70a6fce1fbefec1c984e2a
Alejandro Galvez <159497405+alejalvez@users.noreply.github.com>
2024-10-30 12:38:06 -0500

    Basic encryption and login
    Hashed passwords every time you sign up and then finished initial login

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,4 +1,17 @@
 const User = require("../models/authModel.js");
+const bcrypt = require('bcrypt')
+
+
+const hashPassword = async (password) => {
+  const saltRounds = 10;
+  const hashedPassword = await bcrypt.hash(password, saltRounds);
+  return hashedPassword;
+}
+
+const comparePasswords = async (password, hashed) => {
+  return await bcrypt.compare(password, hashed)
+}
+
 
 const signup = async (req, res) => {
   const { name, password } = req.body
@@ -9,16 +22,36 @@ const signup = async (req, res) => {
   }
 
   try {
+    const hashedPassword = await hashPassword(password)
     const user = await User.create({
-      name, password
+      name, password: hashedPassword
     })
     return res.status(201).json({ message: "User created successfully" })
 
   }
   catch (err) {
-    return res.status(500).json({ message: "Error creating user", error });
+    return res.status(500).json({ message: "Error creating user", err });
   }
 
 };
 
-module.exports = { signup }
+const login = async (req, res) => {
+  try {
+    const { name, password } = req.body
+    const user = await User.findOne({ name })
+    if (!user) {
+      return res.status(400).json({ message: "Username not found" })
+    }
+    const match = await comparePasswords(password, user.password)
+    if (!match) {
+      return res.status(400).json({ message: "Invalid password" })
+    }
+    else {
+      return res.status(201).json({ message: "Login successful" })
+    }
+  }
+  catch (err) {
+    return res.status(500).json({ message: "Error logging in", error })
+  }
+}
+module.exports = { signup, login }
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -4,6 +4,8 @@ const cors = require('cors')
 const authController = require("../controllers/authController");
 
 
+
 router.post("/signup", authController.signup);
+router.post("/login", authController.login)
 
 module.exports = router;
