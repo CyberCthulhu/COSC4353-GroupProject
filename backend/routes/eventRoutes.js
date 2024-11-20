48391fb9ea2bc09e2602cbbff45d3261f1876c23
Chris Morales Barcelo <csmorales2002@gmail.com>
2024-11-20 01:00:23 -0600

    admin event edit
    admins can edit events

--- backend/routes/eventRoutes.js
+++ backend/routes/eventRoutes.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const {createNewEvent, getEvents, getEventById} = require("../controllers/eventController");
+const {createNewEvent, getEvents, getEventById, deleteEventById, updateEventById} = require("../controllers/eventController");
 
 router.get("/events", getEvents);
 router.get("/events/:eventId", getEventById);
 router.post("/events", createNewEvent);
+router.delete("/events/:eventId", deleteEventById);
+router.put("/events/:eventId", updateEventById);
+
 
 module.exports = router;
 
